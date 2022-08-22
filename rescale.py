329f9825de399f3ab7a14b2a1f144dce160323f5
Kim sehyun <dnjfdid14@naver.com>
2022-08-22 17:03:50 +0900

    파일 크기 조정

--- rescale.py
+++ rescale.py
@@ -1,1 +1,27 @@
 import cv2 as cv
+
+image = cv.imread('Images/dogeJPG.jpg')
+video = cv.VideoCapture('Videos/LotteBigbar.mp4')
+cv.imshow('Doge', image)
+
+def rescaleFile(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+resized_image = rescaleFile(image)
+cv.imshow('Resized Doge', resized_image)
+while True:
+    isTrue, frame = video.read()
+    frame_resized = rescaleFile(frame)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+video.release()
+cv.destroyAllWindows()
