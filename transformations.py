111f0ff5dbb4cdad2abe7c4f3e3c2b70164174bc
Kim sehyun <dnjfdid14@naver.com>
2022-08-23 17:02:27 +0900

    이미지 변환

--- transformations.py
+++ transformations.py
@@ -4,7 +4,7 @@ import numpy as np
 img = cv.imread('Images/rtx3090ti.jpg')
 cv.imshow('RTX', img)
 
-# 이미지 번역
+# 이미지 이동
 def translate(img, x, y):
     transMat = np.float32([[1,0,x],[0,1,y]])
     dimentions = (img.shape[1], img.shape[0])
@@ -17,4 +17,19 @@ def translate(img, x, y):
 
 translated = translate(img, 100, 100) # 이미지를 오른쪽으로 100칸 아래로 100칸
 cv.imshow('TranslatedRTX', translated)
+
+# 이미지 회전
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None: # 설정된 회전점이 없으면 중심으로 회전한다 가정
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimentions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimentions)
+
+rotated = rotate(img, 45) # 오른쪽으로 45도 회전
+cv.imshow('SpinRTX', rotated)
 cv.waitKey(0)
