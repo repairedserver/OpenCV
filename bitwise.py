8ed85943af9317ac3a5a5c4bc50924d2b0cc58f3
Kim sehyun <dnjfdid14@naver.com>
2022-08-25 17:30:33 +0900

    비트 연산

--- bitwise.py
+++ bitwise.py
@@ -8,4 +8,20 @@ circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
 
 cv.imshow('Rectangle', rectangle)
 cv.imshow('Circle', circle)
+
+# 비트연산 AND
+bitwiseAND = cv.bitwise_and(rectangle, circle) # 두 이미지의 교차 영역만을 반환
+cv.imshow('BitwiseAND', bitwiseAND)
+
+# 비트연산 OR
+bitwiseOR = cv.bitwise_or(rectangle, circle) # 교차 영역과 그렇지 않은 부분도 모두 반환
+cv.imshow('BitwiseOR', bitwiseOR)
+
+# 비트연산 XOR
+bitwiseXOR = cv.bitwise_xor(rectangle, circle) # 교차 영역을 제외한 모두를 반환
+cv.imshow('BitwiseXOR', bitwiseXOR)
+
+# 비트연산 NOT
+bitwiseNOT = cv.bitwise_not(rectangle) # 색상 반전
+cv.imshow('BitwiseNOT', bitwiseNOT)
 cv.waitKey(0)
