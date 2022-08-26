8429a18e6100d067f125c9aa20b9c61d3e1a6d3b
Kim sehyun <dnjfdid14@naver.com>
2022-08-26 14:56:09 +0900

    히스토그램 계산

--- histogram.py
+++ histogram.py
@@ -1,8 +1,25 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
 
-img = cv.imread('Images/rtx3090ti.jpg')
-cv.imshow('RTX', img)
+img = cv.imread('Images/iphone.jpg')
+cv.imshow('image', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('GrayRTX', gray)
+cv.imshow('Grayimage', gray)
+
+# Grayscale Histogram
+gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+
+# 픽셀 분포
+plt.figure()
+plt.title('Grayscale Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
+
 cv.waitKey(0)
