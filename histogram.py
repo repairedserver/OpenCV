653f3c40df0b824de9481eafc6d56eb1320f8696
Kim sehyun <dnjfdid14@naver.com>
2022-08-26 15:20:48 +0900

    히스토그램 계산

--- histogram.py
+++ histogram.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv.imread('Images/iphone.jpg')
+img = cv.imread('Images/dogeJPG.jpg')
 cv.imshow('image', img)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
@@ -10,16 +10,34 @@ blank = np.zeros(img.shape[:2], dtype='uint8')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Grayimage', gray)
 
+mask = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Mask', masked)
+
 # Grayscale Histogram
-gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+# gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
 
 # 픽셀 분포
+# plt.figure()
+# plt.title('Grayscale Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0,256])
+# plt.show()
+
+# Color Histogram
 plt.figure()
 plt.title('Grayscale Histogram')
 plt.xlabel('Bins')
 plt.ylabel('# of pixels')
-plt.plot(gray_hist)
-plt.xlim([0,256])
-plt.show()
 
+colors = ('b', 'g', 'r')
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], mask, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
+
+plt.show()
 cv.waitKey(0)
