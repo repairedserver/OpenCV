92675cdea871a32970dbdf3204823b4d8a92c23d
Kim sehyun <dnjfdid14@naver.com>
2022-08-29 22:21:07 +0900

    사람 얼굴 감지

--- face_detect.py
+++ face_detect.py
@@ -1,9 +1,29 @@
 import cv2 as cv
 
-img = cv.imread('Faces/jammin.jpeg')
-cv.imshow('Jammin', img)
+# 개인사진
+# img = cv.imread('Faces/yee.jpg')
+# cv.imshow('Jammin', img)
+
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('GrayJammin', gray)
+
+
+# 단체사진
+img = cv.imread('Faces/miss.jpg')
+cv.imshow('Miss', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('GrayJammin', gray)
+cv.imshow('GrayMiss', gray)
 
+haar_cascade = cv.CascadeClassifier('haar_face.xml') 
+# XML 코드의 33000줄을 읽고 변수에 저장
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+# 해당 이미지의 좌표를 잡고 사각형을 그림
+print(f'찾은 얼굴의 수 : {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Faces', img)
 cv.waitKey(0)
